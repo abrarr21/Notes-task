3aacc8701b316762d9ff121886cf2a1c26afe3aa
abrarr21 <abrarrr2110@gmail.com>
2026-05-27 16:36:15 +0530

    get all notes route

--- notes-first-time/src/server.ts
+++ notes-first-time/src/server.ts
@@ -9,41 +9,52 @@ app.get("/", (req, res) => {
 });
 
 // @route /api/notes
-// @title Title of the note
-// @description Create description of the Note
+// @description Create title and description of the Note
 // @access Public
 app.post("/api/notes", async (req, res) => {
   const { title, description } = req.body;
 
   if (!title) {
-    res.status(400).json({
+    return res.status(400).json({
       message: "Title is required",
     });
   }
 
   if (title.trim().length < 3) {
-    res.status(400).json({
+    return res.status(400).json({
       message: "Title must be at least 3 characters long",
     });
   }
 
   if (!description) {
-    res.status(400).json({
+    return res.status(400).json({
       message: "description is required",
     });
   }
 
   if (description.trim().length < 10) {
-    res.status(400).json({
+    return res.status(400).json({
       message: "description must be at least 10 characters long",
     });
   }
 
   const newNote = await notesModel.create({ title, description });
 
-  res.status(200).json({
+  return res.status(201).json({
     message: "Note created successfully",
     note: newNote,
+  });
+});
+
+// @route /api/notes
+// @description Get all the notes
+// @access Public
+app.get("/api/notes", async (req, res) => {
+  const notes = await notesModel.find();
+
+  return res.status(200).json({
+    message: "Notes fetched successfully",
+    notes: notes,
   });
 });
 
