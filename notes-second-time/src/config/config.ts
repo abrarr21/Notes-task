977ab8f6c5ed6020794948760fe96bc5453a2980
abrarr21 <abrarrr2110@gmail.com>
2026-05-27 17:15:28 +0530

    add mongodb connection

---
+++ notes-second-time/src/config/config.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+if (!process.env.MONGODB_URI) {
+  throw new Error("MONGODB_URI is not provided in the .env file");
+}
+
+const config = {
+  MONGODB_URI: process.env.MONGODB_URI,
+};
+
+export default config;
